33ed985280dc63c461a2c1b0b1957da3102e8a55
Ruairí Murphy <ruairifmurphy@gmail.com>
2022-04-12 19:44:09 +0100

    add server side javascript

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
+const multer  = require('multer');
+const fs = require('fs');
 var router = express.Router();
+
+const storage = multer.diskStorage(
+  {
+      destination: './sound_files/',
+      filename: function (req, file, cb ) {
+          cb( null, file.originalname);
+      }
+  }
+);
+
+const upload = multer( { storage: storage } );
+router.use(express.static('./'));
+router.post("/notes", upload.single("audio_data"), function(req,res){
+  const spawn = require("child_process").spawn;
+  const pythonProcess = spawn('python',["application/test_output.py"]);
+  pythonProcess.stdout.on("data", function(data) {
+    console.log(data.toString());
+    res.write(data);
+    res.status(200).send("ok");
+  })
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Tunder' });
 });
+
 
 module.exports = router;
