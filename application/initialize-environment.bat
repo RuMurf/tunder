132d90a52c7bc02b2bab7ccd9b3949b9858da18b
Ruairí Murphy <ruairifmurphy@gmail.com>
2022-04-12 22:35:25 +0100

    add initialize-environment batch script to set up virtual environment and configure PYTHONHOME

---
+++ application/initialize-environment.bat
@@ -0,0 +1,8 @@
+@echo off
+:: Intialize python virtual environment
+python -m venv venv
+:: Add root directory of application to PYTHONPATH
+echo: >> venv\Scripts\activate.bat
+echo set PYTHONPATH=%%cd%%;%%PYTHONPATH%% >> venv\Scripts\activate.bat
+:: Activate virtual environment
+venv\Scripts\activate.bat
--- application/install-dependencies.bat
+++ application/install-dependencies.bat
@@ -1,9 +1,9 @@
-@ECHO OFF
-TITLE Install Dependencies
+@echo off
+title Install Dependencies
 :: Install dependencies from requirements.txt
 pip install -r requirements.txt
-:: Madmom is a special little madam and won't register the fact her dependencies 
+:: Madmom is a special little madam and won't register the fact that her dependencies 
 :: are installed if she's installed from the same command as said dependencies.
-:: Hence, she has to be installed from a separate command.
-:: Making this batch file necessary and my life even more difficult than it already had to be.
+:: Hence, she has to be installed from a separate command,
+:: making this batch file necessary and my life even more difficult than it already had to be.
 pip install madmom
