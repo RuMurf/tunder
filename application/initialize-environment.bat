bdc596fdd70966929abf0687a117d1add3f12ea3
Ruairí Murphy <ruairifmurphy@gmail.com>
2022-04-12 22:42:45 +0100

    fix typo in initialize-environment.bat

--- application/initialize-environment.bat
+++ application/initialize-environment.bat
@@ -3,6 +3,6 @@
 python -m venv venv
 :: Add root directory of application to PYTHONPATH
 echo: >> venv\Scripts\activate.bat
-echo set PYTHONPATH=%%cd%%;%%PYTHONPATH%% >> venv\Scripts\activate.bat
+echo set PYTHONPATH=%cd%;%%PYTHONPATH%% >> venv\Scripts\activate.bat
 :: Activate virtual environment
 venv\Scripts\activate.bat
