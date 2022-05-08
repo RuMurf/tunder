bd3f476865e8154ce3b67ae1fb6fa4f64fb34035
Ruairí Murphy <ruairifmurphy@gmail.com>
2022-05-08 15:28:19 +0100

    updating changes

---
+++ application/test/test_matches.py
@@ -0,0 +1,8 @@
+# PARAMETERS
+# sample_rates = [44100]
+footprint_sizes = [100, 60, 30]
+target_starts = [0.05, 1]
+target_heights = [1500, 3000, 4000]
+target_widths = [2, 4, 6]
+frames_per_seconds = [5, 10, 50]
+peak_thresholds = [20, 10, 0]
