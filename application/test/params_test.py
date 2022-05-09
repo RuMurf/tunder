584ba52df0a861da3794d8d2038d4956ca017b27
Ruairí Murphy <ruairifmurphy@gmail.com>
2022-05-09 20:51:37 +0100

    ......it actually works

--- application/test/params_test.py
+++ application/test/params_test.py
@@ -1,6 +1,16 @@
 from utils import *
 
-def database():
-    add_fingerprint_to_db("")
-print(match_from_file("sound_files/sound-file-1652037574593.wav", "test_params", "test1"))
+def add_db():
+    add_fingerprint_to_db("audio/db/01 Highway To Hell.wav", 60*3+28, db_name="test", collection_name="test", track_id=1)
+    print("Done 1")
+    add_fingerprint_to_db("audio/db/09 Trouble (with a Capital 'T').wav", 60*3+27, db_name="test", collection_name="test", track_id=2)
+    print("Done 2")
+    add_fingerprint_to_db("audio/db/14 N17.wav", 60*4+36, db_name="test", collection_name="test", track_id=3)
+    print("Done 3")
+    add_fingerprint_to_db("audio/db/17 Good Riddance (Time Of Your Life).wav", 60*2+34, db_name="test", collection_name="test", track_id=4)
+    print("Done 4")
 
+def match():
+    print(match_from_file("sound_files/sound-file-1652125736511.wav", "test", "test"))
+
+match()
--- application/test/utils.py
+++ application/test/utils.py
@@ -7,13 +7,14 @@ import math
 from matplotlib import pyplot as plt
 
 # Defaults
-SAMPLE_RATE = 22050
+SAMPLE_RATE = 44100
 FOOTPRINT_SIZE = 30
 TARGET_START = 0.05
 TARGET_HEIGHT = 4000
 TARGET_WIDTH = 2
 FRAMES_PER_SECOND = 5
 PEAK_THRESHOLD = 0
+PEAK_FACTOR = 100
 CONNECTION_STRING = "mongodb://localhost:27017"
 DB = "tunder"
 COLLECTION = "fingerprints"
@@ -43,6 +44,29 @@ def generate_starmap(spectrogram, footprint=FOOTPRINT_SIZE, peak_threshold=PEAK_
                 peaks_y.append(j)
     return peaks, peaks_x, peaks_y
 
+def generate_ordered_starmap(spectrogram, track_length, footprint=FOOTPRINT_SIZE, peak_threshold=PEAK_THRESHOLD, peak_factor=PEAK_FACTOR):
+    filtered_spectrogram = maximum_filter(spectrogram, footprint)
+    peaks = {}
+    peaks_x = []
+    peaks_y = []
+    for i in range(spectrogram.shape[0]):
+        for j in range(spectrogram.shape[1]):
+            if spectrogram[i][j] == filtered_spectrogram[i][j] and filtered_spectrogram[i][j] > peak_threshold:
+                if spectrogram[i][j] not in peaks:
+                    peaks[spectrogram[i][j]] = []
+                peaks[spectrogram[i][j]].append([i,j])
+                peaks_x.append(i)
+                peaks_y.append(j)
+    sorted_peaks = dict(sorted(peaks.items(), reverse=True))
+    max_peaks = []
+    for peak in sorted_peaks.items():
+        if len(max_peaks) >= peak_factor * track_length:
+            break
+        for coord in peak[1]:
+            max_peaks.append(coord)
+    print(max_peaks)
+    return max_peaks, peaks_x, peaks_y
+
 def get_hashes(spectrogram, peaks, track_id=None, t_start=TARGET_START, t_height=TARGET_HEIGHT, t_width=TARGET_WIDTH, fps=FRAMES_PER_SECOND):
     frequency_bin_size = spectrogram.bin_frequencies[1]
 
@@ -67,13 +91,19 @@ def generate_fingerprint(file, track_id=None, sample_rate= SAMPLE_RATE, footprin
     fingerprint = get_hashes(spectrogram, peaks, track_id, t_start=t_start, t_height=t_height, t_width=t_width, fps=fps)
     return fingerprint
 
-def add_fingerprint_to_db(file, db_name=DB, collection_name=COLLECTION, track_id=None, sample_rate= SAMPLE_RATE, footprint=FOOTPRINT_SIZE, fps=FRAMES_PER_SECOND, t_start=TARGET_START, t_height=TARGET_HEIGHT, t_width=TARGET_WIDTH, peak_threshold=PEAK_THRESHOLD):
+def generate_ordered_fingerprint(file, track_length, peak_factor=PEAK_FACTOR, track_id=None, sample_rate= SAMPLE_RATE, footprint=FOOTPRINT_SIZE, fps=FRAMES_PER_SECOND, t_start=TARGET_START, t_height=TARGET_HEIGHT, t_width=TARGET_WIDTH, peak_threshold=PEAK_THRESHOLD):
+    spectrogram = generate_spectrogram(file, sample_rate=sample_rate, fps=fps)
+    peaks, peaks_x, peaks_y = generate_starmap(spectrogram, footprint=footprint, peak_threshold=peak_threshold)
+    fingerprint = get_hashes(spectrogram, peaks, track_id, t_start=t_start, t_height=t_height, t_width=t_width, fps=fps)
+    return fingerprint
+
+def add_fingerprint_to_db(file, track_length, peak_factor=PEAK_FACTOR, db_name=DB, collection_name=COLLECTION, track_id=None, sample_rate= SAMPLE_RATE, footprint=FOOTPRINT_SIZE, fps=FRAMES_PER_SECOND, t_start=TARGET_START, t_height=TARGET_HEIGHT, t_width=TARGET_WIDTH, peak_threshold=PEAK_THRESHOLD):
     collection = get_collection(db_name=db_name, collection_name=collection_name)
     if collection.find_one({"track_id": track_id}):
         print("Error: collection "+collection_name+" already contains track id "+str(track_id))
         return
     
-    fingerprint = generate_fingerprint(file, track_id, sample_rate=sample_rate, footprint=footprint, fps=fps, t_start=t_start, t_height=t_height, t_width=t_width, peak_threshold=peak_threshold)
+    fingerprint = generate_ordered_fingerprint(file, track_length, peak_factor=peak_factor, track_id=track_id, sample_rate=sample_rate, footprint=footprint, fps=fps, t_start=t_start, t_height=t_height, t_width=t_width, peak_threshold=peak_threshold)
     if len(fingerprint) > 1:
         collection.insert_many(fingerprint)
 
@@ -88,25 +118,27 @@ def match_fingerprint(fingerprint, collection):
                     time_ds[db_hash["track_id"]] = []
                 time_ds[db_hash["track_id"]].append(db_hash["time_d"] - f_hash["time_d"])
 
-    # initialise dictionary to store largest bin of time_ds as percentage for each db track
+    # initialise dictionary to store count of largest bin of time_ds as percentage for each db track
     match_results = {}
     for key in time_ds:
         match_results[key] = 0
 
     # generate histogram of time_ds for each db track and calculate majority bin (as percentage)
     if len(match_results) > 1:
+        # generate histograms
         fig, figs = plt.subplots(1, len(time_ds), sharex=True, sharey=True)
-        for i in range(len(figs)):
-            figs[i].set_title("Track "+str(i+1))
-            n, bins, patches = figs[i].hist(time_ds[i+1])
+        for i, key in zip(range(len(figs)), time_ds.keys()):
+            figs[i].set_title("Track "+str(key))
+            n, bins, patches = figs[i].hist(time_ds[key])
             if not math.isnan(max(n)):
-                match_results[i] = max(n)
+                match_results[key] = max(n)
+
         best_match = 0
         result = 0
-        for i in range(len(match_results)):
-            if match_results[i] > best_match:
-                best_match = match_results[i]
-                result = i+1
+        for key in match_results.keys():
+            if match_results[key] > best_match:
+                best_match = match_results[key]
+                result = key
     else:  # only one track with matching hash(es)
         result = match_results.popitem()[0]
 
